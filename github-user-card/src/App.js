127a8f0cbfaa4355a4d654d765ac3dec0302f941
Sandra Coburn <sandra_coburn@hotmail.com>
2020-05-04 20:13:12 -0700

    Added Cors proxy to get rid of cors errors

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -13,51 +13,66 @@ class App extends React.Component {
       cardInfo: [],
       followersInfo: [],
       followerInfo: [],
-      query: ""
+      query: "",
     };
   }
 
   componentDidMount() {
     axios
-      .get("https://api.github.com/users/SandraCoburn")
-      .then(res => {
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.github.com/users/SandraCoburn"
+      )
+      .then((res) => {
         this.setState({
-          cardInfo: res.data
+          cardInfo: res.data,
         });
-        console.log(res);
+        console.log("my info", res);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
+    //This call will get the contributions shart, added the cors proxy to get rid of cors errors
     axios
-      .get("https://api.github.com/users/SandraCoburn/followers")
-      .then(res => {
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://github.com/users/SandraCoburn/contributions"
+      )
+      .then((res) => {
+        console.log("contributions data", res);
+      })
+      .catch((err) => console.log(err));
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.github.com/users/SandraCoburn/followers"
+      )
+      .then((res) => {
         this.setState({
-          followersInfo: res.data
+          followersInfo: res.data,
         });
         console.log("followers", res);
-        //This will go through all followers and make an API call for each of therm to get personal info
-        res.data.forEach(person => {
+        //This will go through all followers and make an API call for each of them to get personal info
+        res.data.forEach((person) => {
           axios
-            .get(`https://api.github.com/users/${person.login}`)
-            .then(res => {
+            .get(
+              `https://cors-anywhere.herokuapp.com/https://api.github.com/users/${person.login}`
+            )
+            .then((res) => {
               //"joined" is a new array to pass info from state, one follower at a time
-              var joined = this.state.followerInfo.concat(res.data);
+              let joined = this.state.followerInfo.concat(res.data);
               this.setState({
-                followerInfo: joined
+                followerInfo: joined,
               });
               console.log("follower", res);
             });
         });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
-  //this get triggered by the input's onchange
-  handleChanges = e => {
+  //this gets triggered by the input's onchange
+  handleChanges = (e) => {
     this.setState({ query: e.target.value });
     this.filterUsers(e.target.value);
   };
   //query is the target value from input. this will filter all the cards and leave only the matching to input data
-  filterUsers = query => {
-    const results = this.state.followerInfo.filter(user =>
+  filterUsers = (query) => {
+    const results = this.state.followerInfo.filter((user) =>
       user.name.toLowerCase().includes(query.toLowerCase())
     );
     this.setState({ followerInfo: results });
